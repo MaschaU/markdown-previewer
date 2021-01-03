373990e7cc242c02f50e7b2b47aaac5edd48b32f
Masa Utkovic <masa.utkovic@gmail.com>
2021-01-03 09:35:44 +0100

    add sub headings

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ export default class App extends React.Component {
       <div className="App">
         <div className="container">
           <h1>Markdown Previewer</h1>
+          <div className="text-area">
+            <h2>Markdown Input</h2>
+          </div>
+          <div className="preview">
+            <h2>Preview</h2>
+          </div>
         </div>
 
       </div>
