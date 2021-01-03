4a1b436de06cf9cd7f253c8c6ae4d4801043141b
Masa Utkovic <masa.utkovic@gmail.com>
2021-01-03 09:29:44 +0100

    add heading

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default class App extends React.Component {
   render () {
     return (
       <div className="App">
+        <div className="container">
+          <h1>Markdown Previewer</h1>
+        </div>
 
       </div>
     );
