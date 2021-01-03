095127585afd1238e704fa9249ced9edc9df97df
Masa Utkovic <masa.utkovic@gmail.com>
2021-01-03 09:54:58 +0100

    add styling to textarea

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ export default class App extends React.Component {
           <h1>Markdown Previewer</h1>
           <div className="markdown-input">
             <h2>Markdown Input</h2>
+            <div className="mark-input">
+              <textarea className="input"></textarea>
+            </div>
           </div>
           <div className="preview">
             <h2>Preview</h2>
           </div>
-          <div className="mark-input">
-            <textarea className="input"></textarea>
-          </div>
+          
         </div>
 
       </div>
